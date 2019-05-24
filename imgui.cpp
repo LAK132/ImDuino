ab8bb6d3ef72b2f1b3e6adbc42f1954482544247
LAK132 <lakridesagain@gmail.com>
2019-05-24 18:22:00 +0930

    Initial commit

---
+++ imgui.cpp
@@ -0,0 +1,6 @@
+#include "imgui.h"
+
+#include "imgui/imgui.cpp"
+#include "imgui/imgui_draw.cpp"
+#include "imgui/imgui_widgets.cpp"
+#include "imgui/examples/imgui_impl_softraster.cpp"
---
+++ imgui.h
@@ -0,0 +1,7 @@
+#ifndef IMDUINO_IMGUI_H
+#define IMDUINO_IMGUI_H
+
+#include "imgui/imgui.h"
+#include "imgui/examples/imgui_impl_softraster.h"
+
+#endif // IMDUINO_IMGUI_H
